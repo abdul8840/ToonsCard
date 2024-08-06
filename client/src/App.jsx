b9f1ebb24b9c7b827a778bace72e9cf379f6e28c
abdul8840 <abdul14941naseer@gmail.com>
2024-08-06 20:27:48 +0530

    complete create category functionality with image

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,6 +10,7 @@ import 'flowbite/dist/flowbite.css';
 import PrivateRoute from './components/PrivateRoute';
 import OnlyAdminPrivateRoute from './components/OnlyAdminPrivateRoute';
 import CreatePost from './pages/CreatePost';
+import CreateCategory from './pages/CreateCategory';
 
 const App = () => {
   return (
@@ -24,6 +25,7 @@ const App = () => {
       </Route>
       <Route element={<OnlyAdminPrivateRoute />}>
           <Route path='/create-post' element={<CreatePost />} />
+          <Route path='/create-category' element={<CreateCategory />} />
       </Route>
       <Route path='/all-cards' element={<AllCards />} />
     </Routes>
