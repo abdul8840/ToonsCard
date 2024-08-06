ff735b5cae3da14d4289d1a848ddc6fd8c038b9c
abdul8840 <abdul14941naseer@gmail.com>
2024-08-06 17:45:17 +0530

    create signup api route

---
+++ api/controllers/auth.controller.js
@@ -0,0 +1,34 @@
+import User from '../models/user.model.js';
+import bcryptjs from 'bcryptjs';
+
+export const signup = async (req, res, next) => {
+    const { name, username, email, password } = req.body;
+
+    if (
+      !name ||
+      !username ||
+      !email ||
+      !password ||
+      username === '' ||
+      email === '' ||
+      password === ''
+    ) {
+      next(errorHandler(400, 'All fields are required'));
+    }
+
+    const hashedPassword = bcryptjs.hashSync(password, 10);
+
+    const newUser = new User({
+      name,
+      username,
+      email,
+      password: hashedPassword,
+    });
+
+    try {
+      await newUser.save();
+      res.json('Signup successful');
+    } catch (error) {
+      next(error);
+    }
+  };
