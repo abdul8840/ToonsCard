f0dd483abb273156637686f4c2e82c892b40f318
abdul8840 <abdul14941naseer@gmail.com>
2024-08-06 19:55:49 +0530

    only admin can access private page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,6 +8,8 @@ import Dashboard from './pages/Dashboard';
 import AllCards from './pages/AllCards';
 import 'flowbite/dist/flowbite.css';
 import PrivateRoute from './components/PrivateRoute';
+import OnlyAdminPrivateRoute from './components/OnlyAdminPrivateRoute';
+import CreatePost from './pages/CreatePost';
 
 const App = () => {
   return (
@@ -19,7 +21,10 @@ const App = () => {
       <Route path='/sign-up' element={<SignUp />} />
       <Route element={<PrivateRoute />}>
           <Route path='/dashboard' element={<Dashboard />} />
-        </Route>
+      </Route>
+      <Route element={<OnlyAdminPrivateRoute />}>
+          <Route path='/create-post' element={<CreatePost />} />
+      </Route>
       <Route path='/all-cards' element={<AllCards />} />
     </Routes>
     </BrowserRouter>
