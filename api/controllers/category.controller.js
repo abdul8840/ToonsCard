11af0d5bd43939a506a382e5ead37f53dc6eb19d
abdul8840 <abdul14941naseer@gmail.com>
2024-08-06 20:38:59 +0530

    create get category api route

--- api/controllers/category.controller.js
+++ api/controllers/category.controller.js
@@ -19,3 +19,45 @@ export const create = async (req, res, next) => {
     next(error);
   }
 };
+
+export const getcategory = async (req, res, next) => {
+  try {
+    const startIndex = parseInt(req.query.startIndex) || 0;
+    const limit = parseInt(req.query.limit) || 9;
+    const sortDirection = req.query.order === 'asc' ? 1 : -1;
+    const categories = await Category.find({
+      ...(req.query.userId && { userId: req.query.userId }),
+      ...(req.query.categoryId && { _id: req.query.categoryId }),
+      ...(req.query.searchTerm && {
+        $or: [
+          { name: { $regex: req.query.searchTerm, $options: 'i' } },
+        ],
+      }),
+    })
+      .sort({ updatedAt: sortDirection })
+      .skip(startIndex)
+      .limit(limit);
+
+    const totalCategory = await Category.countDocuments();
+
+    const now = new Date();
+
+    const oneMonthAgo = new Date(
+      now.getFullYear(),
+      now.getMonth() - 1,
+      now.getDate()
+    );
+
+    const lastMonthCategories = await Category.countDocuments({
+      createdAt: { $gte: oneMonthAgo },
+    });
+
+    res.status(200).json({
+      categories,
+      totalCategory,
+      lastMonthCategories,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
--- api/routes/category.route.js
+++ api/routes/category.route.js
@@ -1,9 +1,10 @@
 import express from 'express';
 import { verifyToken } from '../utils/verifyUser.js';
-import { create } from '../controllers/category.controller.js';
+import { create, getcategory } from '../controllers/category.controller.js';
 
 const router = express.Router();
 
 router.post('/create', verifyToken, create)
+router.get('/getcategory', getcategory)
 
 export default router;
